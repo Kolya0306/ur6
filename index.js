c7b56763ef0e966f2533087170c68f97b58e0899
Kolya <mykola.hevyuk@gmail.com>
2022-06-06 19:47:52 +0300

    my commit 6

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ application.get("/stations", (reg, res) => {
 })
 
 application.listen(8080, () => {
-    console.log("application is running well");
+    console.log("application is running very well");
 });
