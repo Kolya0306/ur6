b4d231dc164794148a47a0e8b1db18b3e170575f
Kolya <mykola.hevyuk@gmail.com>
2022-06-08 19:50:33 +0300

    ur7

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const { response } = require('express');
 const ex = require('express');
-const fs = require('fs');
+const fs = require('fs.promises');
 
 const application = ex();
 
+application.use(ex.json())
+
 application.get("/stations", (reg, res) => {
-    const stations = fs.readFileSync('./stations.json')
-    res.json(JSON.parse(stations));
+    fs.readFile('./stations.json').then(data => {
+        res.json(JSON.parse(data));
+    })
 })
 
-application.get("/stations2", (reg, res) => {
-    const stations = fs.readFileSync('./stations.json')
-    res.json(JSON.parse(stations));
-})
+application.post('/stations', (req, res) =>{
+    const newStation = req.body;
+    fs.readFile('./stations.json').then(data => {
+        const stationArray = JSON.parse(data)
+        stationArray.push(newStation);
+        fs.writeFile('./stations.json', JSON.stringify(stationArray)).then(() => {
+            res.sendStatus(200);
+        })    
+    }).catch(err => console.error(err));
+});
 
-application.listen(8080, () => {
+application.listen(8085, () => {
     console.log("application is running good");
 });
