37df28ea6061f0b90373a92cfc0a6aad0deb7645
Kolya <mykola.hevyuk@gmail.com>
2022-06-10 20:02:12 +0300

    12345

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ex = require('express');
+const req = require('express/lib/request');
 const fs = require('fs.promises');
 
 const application = ex();
@@ -21,6 +22,36 @@ application.post('/stations', (req, res) =>{
         })    
     }).catch(err => console.error(err));
 });
+
+ application.delete('/stations/:id', (req, res) =>{
+    fs.readFile('./stations.json').then(data => {
+        const stations = JSON.parse(data).filter(s => s.id != req.params.id);
+        console.log("delete id: ",req.params.id)
+        fs.writeFile('./stations.json', JSON.stringify(stations)).then(() => {
+            res.sendStatus(200);
+        })    
+    }).catch(err => console.error(err))
+ })
+
+ application.get('/stations/:id', (req, res) => {
+    fs.readFile('./stations.json').then(data => {
+        const newArr = JSON.parse(data).filter(s => s.id == req.params.id)
+        if (newArr.length > 0){
+            res.json(newArr[0]);   
+        } else {
+            res.sendStatus(404);
+        }
+        
+    }).catch(err => console.error(err))
+})
+
+application.put('/stations/:id', (req, res) => {
+    console.log(req.body)
+    fs.readFile('./stations.json').then(data => {
+        res.sendStatus(200)
+    }).catch(err => console.error(err))
+})
+
 
 application.listen(8085, () => {
     console.log("application is running good");
